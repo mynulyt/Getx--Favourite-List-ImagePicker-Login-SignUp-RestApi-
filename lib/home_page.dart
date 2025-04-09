f722a10df50eb935c3270c3668dacd67f35b0375
Mynul Alam <mynulalam27@gmail.com>
2025-04-10 00:44:50 +0700

    commit

---
+++ lib/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> fruitList = [
+    'Orange',
+    'Apple',
+    'Banana',
+    'Mango',
+    'Lemon',
+    'Watermelon',
+    'Pineapple',
+    'Graphs',
+    'Guava',
+    'papaya',
+  ];
+  List<String> tempFruitList = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('')),
+      body: ListView.builder(
+        itemCount: fruitList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              onTap: () {
+                if (tempFruitList.contains(fruitList[index].toString())) {
+                  tempFruitList.remove(fruitList[index].toString());
+                } else {
+                  tempFruitList.add(fruitList[index].toString());
+                }
+                setState(() {});
+              },
+              title: Text(index.toString()),
+              subtitle: Text(fruitList[index].toString()),
+              trailing: Icon(
+                Icons.favorite,
+                color:
+                    tempFruitList.contains(fruitList[index].toString())
+                        ? Colors.white
+                        : Colors.red,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:fav_list/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(title: 'Material App', home: HomePage());
+  }
+}

bfa0e384a5e9786295d71075daea2af78087db99
Mynul Alam <mynulalam27@gmail.com>
2025-06-23 23:25:52 +0600

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(title: 'Practice Getx', home: LoginScreen());
+    return GetMaterialApp(title: 'App of Getx', home: LoginScreen());
   }
 }
 
